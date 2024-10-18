53ccf065fe7dba42d23581e4a1b2422ce32465f0
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2024-10-18 10:05:51 +0200

    Modulo de material creado

---
+++ PMDM/angular/pantalla-login/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './components/login/login.component';
+import { MaterialModule } from './modules/material.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ PMDM/angular/pantalla-login/src/app/components/login/login.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent {
+  
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  password = "";
+  email= "";
+
+  mostrarDatos(event: MouseEvent) {
+
+    event.preventDefault();
+
+    console.log(`Email: ${this.email}, Password: ${this.password}`);
+    
+  }
+}
