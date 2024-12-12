af4ba77e927647b4cdd73fa580430c36923652c1
GonzalezCortesSergio <gonzalez.coser24@triana.salesianos.edu>
2024-12-12 20:42:23 +0100

    Terminado

--- PSP/spring/monumentos/src/main/java/com/salesianostriana/dam/monumentos/service/MonumentoService.java
+++ PSP/spring/monumentos/src/main/java/com/salesianostriana/dam/monumentos/service/MonumentoService.java
@@ -14,8 +14,48 @@ public class MonumentoService {
 
     private final MonumentoRepository repository;
 
-    List<Monumento> findAll() {
+    public List<Monumento> findAll(String orden) {
+
+       if(orden.equalsIgnoreCase("asc"))
+           return repository.findAllAsc();
+
+       else if (orden.equalsIgnoreCase("desc"))
+           return repository.findAllDesc();
 
        return repository.findAll();
     }
+
+    public Optional<Monumento> findById(Long id) {
+
+        return repository.findById(id);
+    }
+
+    public Monumento save(Monumento monumento) {
+
+        return repository.save(monumento);
+    }
+
+    public Optional<Monumento> edit(Long id, Monumento monumento) {
+
+        Optional<Monumento> optionalMonumento = repository.findById(id);
+
+        return optionalMonumento.map(m -> {
+            m.setCodigoPais(monumento.getCodigoPais());
+            m.setDescripcion(monumento.getDescripcion());
+            m.setLatitud(monumento.getLatitud());
+            m.setLongitud(monumento.getLongitud());
+            m.setNombreMonumento(monumento.getNombreMonumento());
+            m.setNombreCiudad(monumento.getNombreCiudad());
+            m.setNombrePais(monumento.getNombrePais());
+            m.setPhotoUrl(monumento.getPhotoUrl());
+            return m;
+        });
+    }
+
+    public void delete(Monumento monumento) {
+
+        repository.delete(monumento);
+    }
+
+
 }
