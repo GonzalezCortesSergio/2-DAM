ce0960f127d27b1e136cb2f72edef9ce5ccbf318
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2025-01-08 08:12:09 +0100

    Firebase aplicado en rick and morty app

---
+++ PMDM/angular/rick-and-morty-app/public/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-RWQQDNE6.mjs";import"./chunk-UXMCDMBU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- PMDM/angular/rick-and-morty-app/src/app/app.component.ts
+++ PMDM/angular/rick-and-morty-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rick-and-morty-app';
+
+  firebaseConfig = {
+    apiKey: "AIzaSyBiUpdBNr4hjAjubWdO5LCLj2mmkKgselU",
+    authDomain: "gassapp-535fc.firebaseapp.com",
+    projectId: "gassapp-535fc",
+    storageBucket: "gassapp-535fc.firebasestorage.app",
+    messagingSenderId: "143340053622",
+    appId: "1:143340053622:web:bb3cff0b01f48c559eab57"
+  };
+
+  app = initializeApp(this.firebaseConfig);
 }
