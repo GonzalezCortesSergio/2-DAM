82c2f7d2818a984b9ee7752be166f09217dc8340
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2025-01-22 12:31:01 +0100

    Comenzando el boletín de Python

---
+++ SGE/python/ejercicios_python_1/ejercicio1/main.py
@@ -0,0 +1,68 @@
+import re
+from unicodedata import normalize
+
+def transformar_texto(texto: str):
+
+    texto_nuevo = []
+
+    texto = re.sub(
+        r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1", 
+        normalize( "NFD", texto), 0, re.I
+    )
+
+    texto = normalize( "NFC", texto)
+    
+    
+
+    abecedario_morse = {
+        "A": ".-",
+        "B": "-...",
+        "C": "-.-.",
+        "D": "-..",
+        "E": ".",
+        "F": "..-.",
+        "G": "--.",
+        "H": "....",
+        "I": "..",
+        "J": ".---",
+        "K": "-.-",
+        "L": ".-..",
+        "M": "--",
+        "N": "-.",
+        "Ñ": "-.",
+        "O": "---",
+        "P": ".--.",
+        "Q": "--.-",
+        "R": ".-.",
+        "S": "...",
+        "T": "-",
+        "U": "..-",
+        "V": "...-",
+        "W": ".--",
+        "X": "-..-",
+        "Y": "-.--",
+        "Z": "--..",
+        "1": ".----",
+        "2": "..---",
+        "3": "...--",
+        "4": "....-",
+        "5": ".....",
+        "6": "-....",
+        "7": "--...",
+        "8": "---..",
+        "9": "----.",
+        "0": "-----",
+        " ": " "
+    }
+
+    for letra in texto:
+
+        texto_nuevo.append(abecedario_morse[letra.upper()])
+
+    
+    texto_nuevo = " ".join(texto_nuevo)
+
+    return texto_nuevo
+
+
+
---
+++ SGE/python/ejercicios_python_1/ejercicio1/test.py
@@ -0,0 +1,10 @@
+from main import *
+
+print("Primer texto: ")
+print(transformar_texto("Hola qué tal estás"))
+
+print(" ")
+print(" ")
+
+print("Segundo texto: ")
+print(transformar_texto("Esto es otra prueba para ver qué tal funciona la función de pasar texto natural a código morse"))
