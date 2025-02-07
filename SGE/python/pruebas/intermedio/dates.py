43047259627e2d059fac8fedeebb424dc9cf092d
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2025-02-07 10:09:08 +0100

    Challenges del curso empezado

--- SGE/python/pruebas/intermedio/dates.py
+++ SGE/python/pruebas/intermedio/dates.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 from datetime import time
+from datetime import date
 
 def print_date(date):
     
@@ -11,17 +12,32 @@ def print_date(date):
     print(date.hour)
     print(date.minute)
     print(date.second)
+    print(date.timestamp())
 
 now = datetime.now()
 
 print_date(now)
 
 
-
-timestamp = now.timestamp()
-
-print(timestamp)
-
-year_2023 = datetime(2023, 1, 1)
+year_2023 = datetime(2023, 1, 1, 0)
 
 print_date(year_2023)
+
+
+
+
+current_time = time(8, 23, 46)
+
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+
+
+current_date = date(2025, 4, 12)
+
+current_date = current_date.today()
+
+print(current_date.day)
+print(current_date.month)
+print(current_date.year)
+
---
+++ SGE/python/pruebas/intermedio/list_comprehension.py
@@ -0,0 +1,9 @@
+### List Comprehension ###
+
+my_original_list = [35, 24, 62, 52, 30, 30, 17]
+
+my_list = [i for i in range(80)]
+
+print(my_list)
+
+
