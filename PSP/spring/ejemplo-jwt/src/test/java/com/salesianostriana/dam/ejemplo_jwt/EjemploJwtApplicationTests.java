56cb78a01db1395e766f56a1cbc25ebf37ad353a
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2025-02-11 09:01:27 +0100

    Ejemplo jwt creado

---
+++ PSP/spring/ejemplo-jwt/src/test/java/com/salesianostriana/dam/ejemplo_jwt/EjemploJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemplo_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
