e51e6a570c947d377148963eae616f17141fb7fb
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2025-02-11 13:30:30 +0100

    Que

--- PSP/spring/ejemplo-jwt/src/main/java/com/salesianostriana/dam/ejemplo_jwt/dto/CreateUsuarioDto.java
+++ PSP/spring/ejemplo-jwt/src/main/java/com/salesianostriana/dam/ejemplo_jwt/dto/CreateUsuarioDto.java
@@ -1,4 +1,27 @@
 package com.salesianostriana.dam.ejemplo_jwt.dto;
 
-public record CreateUsuarioDto() {
+import com.salesianostriana.dam.ejemplo_jwt.model.Usuario;
+import com.salesianostriana.dam.ejemplo_jwt.model.UsuarioRol;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.EnumSet;
+
+public record CreateUsuarioDto(
+        String username,
+        String password,
+        String passwordRepeat,
+        String avatar,
+        String nombreCompleto
+) {
+
+    public Usuario toUsuario(PasswordEncoder passwordEncoder, EnumSet<UsuarioRol> roles) {
+
+        return Usuario.builder()
+                .username(this.username())
+                .password(passwordEncoder.encode(this.password()))
+                .avatar(this.avatar())
+                .nombreCompleto(this.nombreCompleto())
+                .roles(roles)
+                .build();
+    }
 }
